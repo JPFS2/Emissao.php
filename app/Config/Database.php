f16c0efae69065261fddf07ae0d4fdabbc10991a
JPFS2 <johnnathanpacifico@gmail.com>
2023-01-02 14:59:45 -0300

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
 		'hostname' => 'localhost',
 		'username' => '',
 		'password' => '',
-		'database' => '',
+		'database' => 'indus306_ativomedia',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
