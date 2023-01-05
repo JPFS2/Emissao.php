fea5a3ff474ef2e9dd17ea1fb1e62ee135f5cb55
Johnnathan Pacífico <106749308+JPFS2@users.noreply.github.com>
2023-01-05 17:31:03 -0300

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -32,7 +32,7 @@ class Database extends Config
 	 */
 	public $default = [
 		'DSN'      => '',
-		'hostname' => 'localhos',
+		'hostname' => 'localhost',
 		'username' => '',
 		'password' => '',
 		'database' => '',
